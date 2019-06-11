b2033ad1c0b3e8cad301735748ab3d00f23ff488
m3c0d3 <m3c0d3@gmail.com>
2019-06-11 23:35:47 +0300

    134. An Important Note about Route Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {
   }
@@ -21,7 +23,7 @@ export class UserComponent implements OnInit {
     // here we setup a subscription when parameters are changed
     // this is needed when component is not recreated but route
     // parameters are changed
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         // this is an arrow function (anonimous function or lambda)
         (params: Params) => {
@@ -29,6 +31,14 @@ export class UserComponent implements OnInit {
           this.user.name = params['name'];
         }
       );
+    // angular destroys this subscription - angular cleans this
+    // subscription whenever this component is destroyed.
+    // Anyways if you need to do some cleanup whenever the component
+    // is destroyed this can be handled in ngOnDestroy.
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
